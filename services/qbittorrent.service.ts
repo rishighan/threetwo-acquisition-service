011293376c6365a4d6f6570e211b89ac50836fe8
Rishi Ghan <rishi.ghan@gmail.com>
2023-09-11 16:04:06 -0400

    📝 Fixed the endpoint

--- services/qbittorrent.service.ts
+++ services/qbittorrent.service.ts
@@ -20,14 +20,14 @@ export default class ProwlarrService extends Service {
 					handler: async (ctx: Context<{}>) => {
 						try {
 							const result = await axios.request({
-								url: `http://192.168.1.183:9696/api/`,
+								url: `http://192.168.1.183:9696/api/v1/history`,
 								method: `GET`,
 								headers: { Accept: "application/json" },
 								params: {
-									apiKey: "163ef9a683874f65b53c7be87354b38b",
+									apikey: "163ef9a683874f65b53c7be87354b38b",
 								}
 							});
-
+							console.log(result);
 							return result.data;
 						} catch(err) {
 							console.log(err);
