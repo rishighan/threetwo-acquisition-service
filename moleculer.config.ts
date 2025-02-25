de2a6abf689bc3bcdfcaf338dc9e7cef9b244090
Rishi Ghan <rishi.ghan@gmail.com>
2025-02-25 16:26:11 -0500

    ∗ Typo in moleculer config

--- moleculer.config.ts
+++ moleculer.config.ts
@@ -35,7 +35,7 @@ const brokerConfig: BrokerOptions = {
 	// Namespace of nodes to segment your nodes on the same network.
 	namespace: "",
 	// Unique node identifier. Must be unique in a namespace.
-	nodeID: "threetwo-metadata-service",
+	nodeID: "threetwo-acquisition-service",
 	// Custom metadata store. Store here what you want. Accessing: `this.broker.metadata`
 	metadata: {},
 
